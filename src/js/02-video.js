44800d79c08055b16cb95812181090401b4ae71b
PolinaTsugankova <pp6056512@gmail.com>
2023-04-03 15:57:21 +0300

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const CURRENT_TIME = 'videoplayer-current-time';
+
+function onPlay({ seconds }) {
+  localStorage.setItem(CURRENT_TIME, seconds);
+}
+
+if (localStorage.getItem(CURRENT_TIME) !== null) {
+  player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
